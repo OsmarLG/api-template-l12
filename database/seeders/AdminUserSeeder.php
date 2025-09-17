66e45c196bbda2755806a9782a1abcea40a02add
Osmar Alejandro Liera Gomez <80195420+OsmarLG@users.noreply.github.com>
2025-09-16 22:22:39 -0700

    Update AdminUserSeeder.php

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -10,7 +10,7 @@ class AdminUserSeeder extends Seeder
 {
     public function run(): void
     {
-        $email = 'admin@chinmex.mx';
+        $email = 'admin@app.com';
         $user = User::where('email', $email)->first();
 
         if (!$user) {
